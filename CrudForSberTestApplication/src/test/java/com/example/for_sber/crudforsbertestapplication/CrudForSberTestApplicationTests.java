07d695fc07a6e2179ddb3fd14aa89923571d26f7
BekmuratSA <bekmuratsatyboldiev98@gmail.com>
2024-05-18 17:48:12 +0300

    FIRST_COMMIT

---
+++ CrudForSberTestApplication/src/test/java/com/example/for_sber/crudforsbertestapplication/CrudForSberTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.for_sber.crudforsbertestapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudForSberTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
